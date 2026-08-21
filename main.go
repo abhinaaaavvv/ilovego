a458e258446eef16cd856fd14d1e576d3ff13f6e
Abhinav Sarkar <abhi.sarkar.anu@gmail.com>
2026-08-21 21:27:31 +0530

    defer

--- main.go
+++ main.go
@@ -1,47 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-	"time"
-)
+import "fmt"
 
 func main() {
-	var sum int = 0
+	defer fmt.Println("world")
+
+	fmt.Print("hello ")
+
 	for i := range 10 {
-		sum += i
-	}
-
-	if sum < 100 && sum > 0 { // expression
-		fmt.Println(1)
-	} else if sum == 0 {
-		fmt.Println(0)
-	} else {
-		fmt.Println(-1)
-	}
-
-	if product := sum * 2; product > sum { //expression; condition
-		fmt.Println(product)
-	}
-
-	fmt.Print("Go is running in: ")
-	switch os := runtime.GOOS; os { //expression; condition - same as if
-	case "darwin":
-		fmt.Println("OS X.")
-	case "linux":
-		fmt.Println("Linux")
-	default:
-		fmt.Printf("%s.\n", os)
-	}
-
-	var t = time.Now()
-
-	switch {
-	case t.Hour() < 12: //expression
-		fmt.Println("Good morning!")
-	case t.Hour() < 17:
-		fmt.Println("Good Afternoon.")
-	default:
-		fmt.Println("Good evening.")
+		defer fmt.Println(i)
 	}
 }
