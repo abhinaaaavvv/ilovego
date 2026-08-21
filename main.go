dc441deec531100e6e478b8d8ed3db38e2d51e8c
Abhinav Sarkar <abhi.sarkar.anu@gmail.com>
2026-08-21 14:06:19 +0530

    for loop

--- main.go
+++ main.go
@@ -2,32 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math"
-
-	"ilovego/public"
-)
-
-func add(a int, b int) (int, int) {
-	return a + b, a * b
-}
-
-var (
-	number = 1234
-	name   = "abhinav"
-)
-
-const (
-	Pi  = math.Pi
-	age = 16
 )
 
 func main() {
-	var a = 5
-	var b = 10
-
-	var sum = public.Add(a, b)
-	var product = public.Multi(a, b)
-
-	fmt.Println("the sum of a and b is", sum)
-	fmt.Println("the product of a and b is", product)
+	var sum int = 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
 }
