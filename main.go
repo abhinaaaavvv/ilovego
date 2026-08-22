936bb1908b14374ffe3519370c987bf3b58ac571
Abhinav Sarkar <abhi.sarkar.anu@gmail.com>
2026-08-22 23:44:09 +0530

    bubble sort

--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func add(a *int, b *int) int {
-	return *a + *b
-}
 func main() {
-	var a = 5
-	var b = 10
+	arr := [10]int{1, 0, 2, 9, 3, 8, 4, 7, 5, 6}
 
-	var sum = add(&a, &b)
+	sorted := sort(arr[:])
 
-	fmt.Println(sum)
+	fmt.Println(sorted)
+}
 
+func sort(arr []int) []int {
+	n := len(arr)
+	if n <= 1 {
+		return arr
+	}
+
+	for i := 0; i < n-1; i++ {
+		swapped := false
+		for j := 0; j < n-1-i; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+				swapped = true
+			}
+		}
+		if !swapped {
+			break
+		}
+	}
+	return arr
 }
