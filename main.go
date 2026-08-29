1ea30ef6f45b00f0aeeffc287ca081539b964a06
Abhinav Sarkar <abhi.sarkar.anu@gmail.com>
2026-08-29 22:24:16 +0530

    pointer receiver and value receiver

--- main.go
+++ main.go
@@ -6,21 +6,26 @@ type Vertex struct {
 	X, Y float64
 }
 
-func (v Vertex) add() float64 {
-	return v.X + v.Y
-}
-
 func (v *Vertex) scale(f float64) {
 	v.X = v.X * f
 	v.Y = v.Y * f
 }
 
-func main() {
-	v := Vertex{
-		5, 6,
-	}
-	fmt.Println(v.add())
+func scaleFunc(v *Vertex, f float64) {
 
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	v := Vertex{5, 6}
 	v.scale(5)
-	fmt.Println(v.add())
+	scaleFunc(&v, 10)
+
+	p := &Vertex{7, 8}
+	p.scale(5)
+	scaleFunc(p, 10)
+
+	fmt.Println(v, p)
+
 }
