73ed9f965b360d606d6d9325e1dd952c378f1ce6
Abhinav Sarkar <abhi.sarkar.anu@gmail.com>
2026-08-29 22:18:17 +0530

    methods with pointers

--- main.go
+++ main.go
@@ -3,16 +3,24 @@ package main
 import "fmt"
 
 type Vertex struct {
-	X, Y int
+	X, Y float64
 }
 
-func (v Vertex) add() int {
+func (v Vertex) add() float64 {
 	return v.X + v.Y
+}
+
+func (v *Vertex) scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
 }
 
 func main() {
 	v := Vertex{
 		5, 6,
 	}
+	fmt.Println(v.add())
+
+	v.scale(5)
 	fmt.Println(v.add())
 }
