40a61e94fb3ae50c66e5492371cb0220a3ad86c4
Abhinav Sarkar <abhi.sarkar.anu@gmail.com>
2026-08-29 14:57:04 +0530

    append

--- main.go
+++ main.go
@@ -5,38 +5,9 @@ import (
 )
 
 func main() {
-	arr := [5]int{1, 2, 3, 4, 5}
-	slice := arr[1:3]
-	fmt.Println(arr)
-	fmt.Println(slice)
+	slice_1 := []int{1, 2, 3, 4, 5}
+	slice_2 := []int{6, 7, 8, 9, 0}
 
-	q := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	fmt.Println(q)
-
-	r := []bool{true, false, false, true, false, true, true}
-	fmt.Println(r)
-
-	s := []struct {
-		i int
-		b bool
-	}{
-		{1, true},
-		{2, false},
-		{q[2], r[2]},
-	}
-	fmt.Println(s)
-
-	t := q[3:]
-	fmt.Println(t)
-
-	u := q[:3]
-	fmt.Println(u)
-
-	v := q[:]
-	fmt.Println(v)
-
-	a := make([]int, 5)
-	b := make([]int, 5, 10)
-	fmt.Println(a, b)
-
+	slice_1 = append(slice_1, slice_2...)
+	fmt.Println(slice_1)
 }
