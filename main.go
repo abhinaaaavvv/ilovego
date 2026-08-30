5996000340a2b9a4939e0574c1ec86926dbd31e4
Abhinav Sarkar <abhi.sarkar.anu@gmail.com>
2026-08-30 21:12:49 +0530

    concurrency - wait groups

--- main.go
+++ main.go
@@ -1,37 +1,95 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
 
-type numbers interface {
-	int | float64 | uint
-}
-
-func Index[T comparable](s []T, x T) int {
-	for i, v := range s {
-		if v == x {
-			return i
-		}
-	}
-	return -1
-}
-
-func Add[N numbers](a ...N) N { //variadic function (a ...N)
-	var sum N = 0
-
-	for _, v := range a {
-		sum += v
-	}
-	return sum
+type Order struct {
+	Id     int
+	Status string
 }
 
 func main() {
-	s1 := []int{1, 2, 3, 4, 5}
-	fmt.Println(Index(s1, 5))
+	wg := sync.WaitGroup{}
+	wg.Add(3)
 
-	s2 := []string{"abhinav", "moni", "bhavani"}
-	fmt.Println(Index(s2, "hello"))
+	orders := generateOrders(20)
 
-	fmt.Println(Add(5, 6))
-	fmt.Println(Add(5.5, 6.7, 9.2))
-	fmt.Println(Add(-5, 6, 14))
+	go func() {
+		processOrders(orders)
+		defer wg.Done()
+	}()
+
+	go func() {
+		updateOrderStatuses(orders)
+		defer wg.Done()
+	}()
+
+	go func() {
+		reportOrderStatus(orders)
+		defer wg.Done()
+	}()
+
+	wg.Wait()
+
+	fmt.Println("All operations completed.")
+}
+
+func updateOrderStatuses(orders []*Order) {
+	for _, order := range orders {
+		time.Sleep(
+			time.Duration(rand.Intn(300)) * time.Millisecond,
+		)
+
+		status := []string{
+			"Pending", "Shipped", "Delivered",
+		}[rand.Intn(3)]
+
+		order.Status = status
+
+		fmt.Printf(
+			"Updated order %v status: %s\n",
+			order.Id, order.Status,
+		)
+	}
+}
+
+func processOrders(orders []*Order) {
+	for _, order := range orders {
+		time.Sleep(
+			time.Duration(rand.Intn(500)) * time.Millisecond,
+		)
+
+		fmt.Printf("Processing order %v\n", order.Id)
+	}
+}
+
+func generateOrders(count int) []*Order {
+	orders := make([]*Order, count)
+
+	for i := range count {
+		orders[i] = &Order{
+			i + 1, "Pending",
+		}
+	}
+
+	return orders
+}
+
+func reportOrderStatus(orders []*Order) {
+	for range 5 {
+		time.Sleep(1 * time.Second)
+		fmt.Println("\n--- Order Status Report ---")
+
+		for _, order := range orders {
+			fmt.Printf(
+				"Order %v: %s\n",
+				order.Id, order.Status,
+			)
+		}
+	}
+	fmt.Println("---------------------------")
 }
